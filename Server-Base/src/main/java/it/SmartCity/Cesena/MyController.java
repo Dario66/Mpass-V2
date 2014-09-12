20f71a786986375b45d9da14d73d13809a1c5644
Dario <dario.grandi@virgilio.it>
2014-09-12 15:53:55 +0200

    Update
    esempio passaggio parametri aggiunto

--- Server-Base/src/main/java/it/SmartCity/Cesena/MyController.java
+++ Server-Base/src/main/java/it/SmartCity/Cesena/MyController.java
@@ -45,4 +45,54 @@ public class MyController {
 	// Normalmente GET è utilizzato per richiedere piccole quantità di dati 
 	// mentre POST per quantità piu importanti(come filmati, audio etc.)
 	
+	
+	
+	
+	
+	
+	
+	
+	//ricordarsi di scrivere lo stesso path anche nella richiesta nel client.
+	//la variabile title deve essere la stessa passata al server
+	@RequestMapping(value="/prova/passaggioparametri", method=RequestMethod.GET)
+	    public @ResponseBody String get(
+	    		@RequestParam ("title")String ciao
+	    		) {
+		 //controlla che sia stato inviata una Stringa contenente "ok"
+		 if(ciao.compareTo("ok")==0){
+		/* Collection<String> list =new ArrayList<String>();
+		//add some stuff
+		list.add("android");
+		list.add("apple");*/
+		
+			return "va bene"; //list;
+		 }
+		 return "non va bene";//null;
+		}
+		/*nell'activity dell'app si può aggiungere il seguente codice dentro la funzione
+		sarà inviata la richiesta con un parametro di tipo String per poi ritornare una Stringa.
+		
+		try{
+		String s = videoSvc.get("ok");
+		ciao.setText(s.toString());
+		 
+		}catch(Exception e){
+			ciao.setText(e.toString());
+			Toast.makeText(getApplicationContext(), e.getCause().toString(), Toast.LENGTH_SHORT).show();
+		}
+		 nella ClientSvcApi inserire la richiesta:
+		 
+		 
+		 @GET("/prova/passaggioparametri")
+		  public String get(@Query("title") String title);
+		  
+		  
+		  
+		  
+		*/
+		
+		
+		
+		
+		
 }
